a1f52d7a318ba7c446a0d7bf096efb8052a4b0fb
Vantchy <wanxjnqi20080614@qq.com>
2026-08-22 09:13:08 +0800

    hw7

--- HW/hw07/hw07.scm
+++ HW/hw07/hw07.scm
@@ -1,15 +1,19 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+
+(define (pow base exp) (cond 
+((zero? exp) 1)
+((even? exp)(pow (square base)(/ exp 2)))
+((odd? exp)(* base (pow(square base)(/ (- exp 1) 2))))))
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
+      (let ((y(repeatedly-cube (- n 1) x)))
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (cadr (cdr s)))
